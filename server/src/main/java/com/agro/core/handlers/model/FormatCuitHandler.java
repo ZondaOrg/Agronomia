f1550abd282559453301f17dbcc49bc087702087
Nicolás Bossi <nbossi@hotmail.com>
2026-09-12 23:21:05 -0300

    Se creó un manejador de excepciones para manejar los errores de la validación del cuit

---
+++ server/src/main/java/com/agro/core/handlers/model/FormatCuitHandler.java
@@ -0,0 +1,36 @@
+package com.agro.core.handlers.model;
+
+import com.agro.shared.valueObjects.cuit.CuitDuplicatedException;
+import com.agro.shared.valueObjects.cuit.CuitFormatException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class FormatCuitHandler extends BuisnessHandlerException<CuitFormatException>{
+
+    @ExceptionHandler(CuitFormatException.class)
+    public ResponseEntity<BuisnessErrorResponse> handleFormatCuit(
+            CuitFormatException exception,
+            HttpServletRequest request
+    ) {
+        return handle(exception, request);
+    }
+
+    @Override
+    protected HttpStatus status() {
+        return HttpStatus.BAD_REQUEST;
+    }
+
+    @Override
+    protected String message() {
+        return "El formato del cuit no es valido";
+    }
+
+    @Override
+    protected String title() {
+        return "Cuit con mal formato";
+    }
+}
