59e165607654af4c92b6724ff9d25a5ec4def937
Nicolás Bossi <nbossi@hotmail.com>
2026-09-15 13:31:57 -0300

    Se modificó la definición del provedor para que se tome como referencia el contrato definido en la api

--- client/src/features/add-provider/pages/AddProvider.tsx
+++ client/src/features/add-provider/pages/AddProvider.tsx
@@ -6,7 +6,7 @@ import { token } from "@styled-system/tokens";
 import { useAddProviders } from "../hook/use-add-provider";
 import Spinner from "@/shared/components/spinner/Spinner";
 import ErrorToast from "@/shared/components/toast/error/ErrorToast";
-import type { ProviderRequest } from "../types/ProviderRequest";
+import type { ProviderRequest } from "../adapter/ProviderRequest";
 import { ModalCreateProvider } from "./components/ModalCreateProvider";
 import { ConfirmModal } from "@/shared/components/modal/variants/ConfirmModalProps";
 import { css } from "@styled-system/css";
