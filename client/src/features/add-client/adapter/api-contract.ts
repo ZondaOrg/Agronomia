713e4217a3d3698e08ec84b0f926f8fa7ec45331
Nicolás Bossi <nbossi@hotmail.com>
2026-09-15 14:41:46 -0300

    Se creó el contrato para agregar un cliente de distinto tipo

---
+++ client/src/features/add-client/adapter/api-contract.ts
@@ -0,0 +1,22 @@
+export const ADD_NATURAL_CLIENT = {
+    name: "name",
+    surname: "surname",
+    phone: "phone",
+    cuit: "cuit",
+    location: "location",
+    province: "province",
+    email: "email",
+    address: "address",
+} as const
+
+export const ADD_LEGAL_NAME = {
+    razonSocial: "razonSocial",
+    associateName: "associateName",
+    associateSurname: "associateSurname",
+    associatePhone: "associatePhone",
+    cuit: "cuit",
+    location: "location",
+    province: "province",
+    email: "email",
+    address: "address",
+} as const
--- client/src/features/add-client/adapter/client-schema.ts
+++ client/src/features/add-client/adapter/client-schema.ts
@@ -1,4 +1,4 @@
-import type { SocialMotiveSchema } from "../pages/types/social-motive-schema";
-import type { NaturalPersonSchema } from "../pages/types/natural-person-schema";
+import type { SocialMotiveSchema } from "../pages/types/social-motive/social-motive-schema";
+import type { NaturalPersonSchema } from "../pages/types/natural-person/natural-person-schema";
 
 export type ClientSchema = SocialMotiveSchema | NaturalPersonSchema;
