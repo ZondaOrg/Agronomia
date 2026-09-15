eecf5f52c2f40843698a7320f5e4838b3320c2a9
Nicolás Bossi <nbossi@hotmail.com>
2026-09-15 15:26:33 -0300

    Se modificó el adaptador de respuesta y la estrctura de las interfaces de dominio

--- client/src/features/add-client/domain/client.ts
+++ client/src/features/add-client/domain/client.ts
@@ -1,7 +1,11 @@
+import type { CompleteName } from "./complete-name"
+
 export interface Client {
     id: number
     cuit: string,
     address?: string
     location: string
     province: string
+    completeName: CompleteName
+    email?: string | null
 }
