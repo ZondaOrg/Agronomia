5318b0ff0e64b2bae01b54aa0c6cad4db76df83c
Nicolás Bossi <nbossi@hotmail.com>
2026-09-17 15:27:03 -0300

    Se creó el tipo para la moneda

--- client/src/features/add-product/types/sub-forms.ts
+++ client/src/features/add-product/types/sub-forms.ts
@@ -1,6 +1,8 @@
 import { createSubForms } from "@/shared/components/forms/types/factory";
 import type { SubFormData } from "@/shared/components/forms/types/sub-form";
 import { ADD_PRODUCT } from "../adapters/request/api-contract";
+import { createSelectOptions } from "@/shared/types/input/input-data/create-select-options";
+import { moneys } from "./money";
 
 const productSubForms: SubFormData[] = createSubForms([
     {
@@ -19,7 +21,7 @@ const productSubForms: SubFormData[] = createSubForms([
         name: "Valores",
         fields: [
             [
-                { motive: "Moneda", name: ADD_PRODUCT.money, type: "select", options: ["a"].map(a => { return  { label: a, value: a, id: a}}) },
+                { motive: "Moneda", name: ADD_PRODUCT.money, type: "select", options: createSelectOptions(moneys)},
                 { motive: "Precio lista", name: ADD_PRODUCT.listPrice }, 
                 { motive: "Iva", name: ADD_PRODUCT.iva, type: "select", options: ["a"].map(a => { return  { label: a, value: a, id: a}}) },
             ],
