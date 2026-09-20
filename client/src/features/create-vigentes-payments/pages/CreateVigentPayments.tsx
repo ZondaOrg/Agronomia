4c312e50bdd8edb7595e9745ba2663a0c6222a77
Tomas Mendoza <tm1453766@gmail.com>
2026-09-20 14:38:03 -0300

    ft: se genera una nueva implementacion del form table, el cual recibe una referencia de funcion y apila las rows que se genera

--- client/src/features/create-vigentes-payments/pages/CreateVigentPayments.tsx
+++ client/src/features/create-vigentes-payments/pages/CreateVigentPayments.tsx
@@ -1,10 +1,8 @@
+import { useRef } from "react";
 import ComposeForm from "@/shared/components/forms/compose-form/ComposeForm";
 import { paymentsSubForms } from "./types/subForms";
 import paymentsSchema from "./types/vigent-schema";
 import { CreatePaymentsTable } from "./components/table/CreatePaymentsTable";
-import type { Payment } from "@/features/get-vigentes-payments-by-provider/types/VigentesPayment";
-import { useDraftRows } from "@/shared/hooks/use-draft-rows";
-import type { VigentPaymentRequest } from "../adapter/PaymentRequest";
 import { useCreateVigentPayments } from "../hook/use-create-vigent-payments";
 import Spinner from "@/shared/components/spinner/Spinner";
 
@@ -13,19 +11,13 @@ type Props = {
 };
 
 export const CreateVigentPayment = ({ providerId }: Props) => {
-    const { table, rows, addDraft, removeDraft, changePage } =
-        useDraftRows<Payment>();
+    const { withPayments, loading, error } =
+        useCreateVigentPayments(providerId);
 
-    const { createVigentPayments, loading, error } = useCreateVigentPayments();
+    const submitRef = useRef<(rest: { nameList: string }) => void>(() => {});
 
-    const onSubmit = (data: Pick<VigentPaymentRequest, "nameList">) => {
-        const payload: VigentPaymentRequest = {
-            nameList: data.nameList,
-            providerId,
-            payments: rows.map(({ data: rowData }) => rowData),
-        };
-
-        createVigentPayments(payload);
+    const onSubmit = (data: { nameList: string }) => {
+        submitRef.current(data);
     };
 
     return (
@@ -39,10 +31,8 @@ export const CreateVigentPayment = ({ providerId }: Props) => {
                 onCancel={() => console.log("cancel")}
             />
             <CreatePaymentsTable
-                table={table}
-                onAddRow={(data) => addDraft(data as Payment)}
-                onRemoveRow={removeDraft}
-                onPageChange={changePage}
+                withPayments={withPayments}
+                submitRef={submitRef}
             />
             {loading && <Spinner />}
             {error && <p>Ocurrió un error al guardar el listado.</p>}
--- client/src/features/create-vigentes-payments/pages/components/table/CreatePaymentsTable.tsx
+++ client/src/features/create-vigentes-payments/pages/components/table/CreatePaymentsTable.tsx
@@ -1,36 +1,14 @@
-import { FormTable } from "@/shared/components/table/form-table/FormTable";
-import type { Table } from "@/shared/types/table/Table";
-import type * as z from "zod";
-import { DeleteButton } from "@/shared/components/button/variants/Delete-button";
 import type { Payment } from "@/features/get-vigentes-payments-by-provider/types/VigentesPayment";
 import paymentSchema from "./types/payment-schema";
 import { paymentInputs } from "./types/Table";
+import { createDraftTable } from "@/shared/components/table/draft-table/draftTable";
 
-type CreatePaymentsTableProps = {
-    table: Table<Payment>;
-    onAddRow: (data: z.infer<typeof paymentSchema>) => void;
-    onRemoveRow: (rowId: number) => void;
-    onPageChange: (newPage: number) => void;
-};
-
-export const CreatePaymentsTable = ({
-    table,
-    onAddRow,
-    onRemoveRow,
-    onPageChange,
-}: CreatePaymentsTableProps) => {
-    return (
-        <FormTable<Payment, typeof paymentSchema>
-            table={table}
-            inputs={paymentInputs}
-            schema={paymentSchema}
-            nameElements="formas de pago"
-            onPageChange={onPageChange}
-            onAddRow={onAddRow}
-            addLabel="+ Añadir forma de pago"
-            renderRowActions={(rowId) => (
-                <DeleteButton onClick={() => onRemoveRow(rowId)} />
-            )}
-        />
-    );
-};
+export const CreatePaymentsTable = createDraftTable<
+    Payment,
+    typeof paymentSchema
+>({
+    inputs: paymentInputs,
+    schema: paymentSchema,
+    nameElements: "formas de pago",
+    addLabel: "+ Añadir forma de pago",
+});
