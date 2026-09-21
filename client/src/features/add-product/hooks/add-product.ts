03cedc729c77a00774a76c66e655a11eca187826
Nicolás Bossi <nbossi@hotmail.com>
2026-09-21 01:06:40 -0300

    Se utilizó el componente de notificación sobre la funcionalidad de agregar el producto

--- client/src/features/add-product/hooks/add-product.ts
+++ client/src/features/add-product/hooks/add-product.ts
@@ -1,11 +1,36 @@
 import useFetch from "@/shared/hooks/use-fetch/useFetch.hook";
 import type { AddedProduct } from "../domain/product";
-import add from "../services/add";
+import useNotify from "@/shared/hooks/use-notify/use-notify";
+import type { AddProductRequest } from "../adapters/request/add-product";
+import type { HttpError } from "@/core/server/errors/http-error";
+import addProduct from "../services/add";
 
 const useAddProduct = () => {
-    const { data, error, execute, refresh } = useFetch<AddedProduct>();
+    const { execute, refresh } = useFetch<AddedProduct>();
 
-    return { data, error, add: execute(add), refresh }
+    const { action, notify, isCancel, handleNotify, handleCancelNotify, init } = useNotify()
+
+    async function add(productData: AddProductRequest) {
+        handleNotify(
+            productData, 
+            {
+                title: "Cliente agregado",
+                message: (product: AddedProduct) => `Se registro el producto ${product.name}`
+            },
+            {
+                title: "Error Cliente",
+                message: (error: HttpError) => error.getMessage
+            },
+            execute(addProduct)
+        )
+    }
+
+    const onRefresh = () => {
+        refresh()
+        init()
+    }
+
+    return { isCancel, notify, action, add, onRefresh, handleCancelNotify };
 }
 
 export default useAddProduct;
--- client/src/features/add-product/pages/AddProduct.tsx
+++ client/src/features/add-product/pages/AddProduct.tsx
@@ -1,64 +1,30 @@
-import useIsModal from "@/shared/hooks/use-is-modal";
 import useAddProduct from "../hooks/add-product";
 import ComposeForm from "@/shared/components/forms/compose-form/ComposeForm";
-import { ConfirmModal } from "@/shared/components/modal/variants/commit/ConfirmModalProps";
-import ErrorToast from "@/shared/components/toast/error/ErrorToast";
-import SuccessToast from "@/shared/components/toast/success/SuccessToast";
-import type { AddProductRequest } from "../adapters/request/add-product";
 import productSubForms from "../types/sub-forms";
 import productSchema from "../types/schema";
-import { SuccessModal } from "@/shared/components/modal/variants/success/ModalCreateClient";
+import NotifyHandler from "@/shared/components/notify/NotifyHandler";
 
 const AddProduct = () => {
-    const { add, refresh, data, error } = useAddProduct();
-    const { isOpen, onOpenIs, backToPrev, refresh: r } = useIsModal();
-
-    const handleProduct = async (data: AddProductRequest) => {
-        const product = await add(data);
-        if (product) onOpenIs(!!product, "confirm");
-    };
+    const { isCancel, notify, action, add, onRefresh, handleCancelNotify } = useAddProduct();
 
     return (
-        <>
+        <NotifyHandler
+            notify={notify}
+            action={action}
+            isCancel={isCancel}
+            isBack={false}
+            refresh={onRefresh}
+            onCancel={handleCancelNotify}
+        >
             <ComposeForm
                 subForms={productSubForms}
                 schema={productSchema}
                 buttonData={{ text: "Agregar producto" }}
-                onSubmit={handleProduct}
+                onSubmit={add}
                 size="lg"
-                onCancel={(isData) => onOpenIs(isData, "advertence")}
+                onCancel={handleCancelNotify}
             />
-            <ConfirmModal
-                isOpen={isOpen("advertence")}
-                title="¿Seguro deseas cancelar?"
-                message="Si cancelas perderás los cambios realizados."
-                confirmText="Abandonar"
-                cancelText="Continuar editando"
-                danger
-                onConfirm={backToPrev}
-                onCancel={r}
-            />
-
-            <SuccessModal
-                isOpen={isOpen("confirm")}
-                onClose={backToPrev}
-                title="Producto agregado"
-                message="El producto ha sido agregado correctamente."
-            />
-            {error && (
-                <ErrorToast
-                    message={error.message}
-                    onClose={refresh}
-                />
-            )}
-            {data && (
-                <SuccessToast
-                    message={`Se ha creado el product ${data.name}`}
-                    title="Producto creado"
-                    onClose={refresh}
-                />
-            )}
-        </>
+        </NotifyHandler>
     );
 }
 
