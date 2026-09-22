5e3e048e21d4b26d1b8d3752089d2478196d21c4
Nicolás Bossi <nbossi@hotmail.com>
2026-09-22 11:22:09 -0300

    Se modeló la clase opcional asociada al producto

---
+++ server/src/main/java/com/agro/feature/product/domain/Optional.java
@@ -0,0 +1,25 @@
+package com.agro.feature.product.domain;
+
+
+import lombok.Getter;
+
+public class Optional {
+
+    private Product product;
+
+    @Getter
+    private String name;
+
+    @Getter
+    private Double price;
+
+    @Getter
+    private Money money;
+
+    public Optional(Product product, String name, Double price) {
+        this.product = product;
+        this.name = name;
+        this.price = price;
+        this.money = product.getMoney();
+    }
+}
