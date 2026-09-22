1f90fe0b8b6d61a666e7fc4dd11c0dc5ebcc6210
Nicolás Bossi <111707809+nicobossi@users.noreply.github.com>
2026-09-22 19:19:06 -0300

    Se formateo el texto del nombre para poder realizar validaciones no distinguiendo maysuculas, minusculas y espacios (#130)
    Co-authored-by: Nicolás Bossi <nbossi@hotmail.com>

--- server/src/main/java/com/agro/feature/product/domain/Product.java
+++ server/src/main/java/com/agro/feature/product/domain/Product.java
@@ -122,6 +122,10 @@ public class Product {
         return name.get();
     }
 
+    public String getFormatName() {
+        return name.getFormatText();
+    }
+
     public Integer getBonification() {
         return bonification.get();
     }
