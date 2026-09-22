6ed74b5c3cd67c9fe241ff80498f4ae05ce9b8d4
Nicolás Bossi <nbossi@hotmail.com>
2026-09-22 15:31:57 -0300

    Se creó un servicio y una interface para el tipo de producto

---
+++ server/src/main/java/com/agro/feature/productType/services/ProductTypeService.java
@@ -0,0 +1,9 @@
+package com.agro.feature.productType.services;
+
+import com.agro.feature.productType.domain.ProductType;
+
+import java.util.Set;
+
+public interface ProductTypeService {
+    Set<ProductType> getAll();
+}
---
+++ server/src/main/java/com/agro/feature/productType/services/impl/ProductTypeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.agro.feature.productType.services.impl;
+
+import com.agro.feature.productType.domain.ProductType;
+import com.agro.feature.productType.services.ProductTypeService;
+
+import java.util.Set;
+
+public class ProductTypeServiceImpl implements ProductTypeService {
+    @Override
+    public Set<ProductType> getAll() {
+        return Set.of();
+    }
+}
