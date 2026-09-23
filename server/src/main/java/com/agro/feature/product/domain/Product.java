cc99f2caf32e4c0cb85dac75b9a88f22a8871eeb
Nicolás Bossi <nbossi@hotmail.com>
2026-09-23 12:45:42 -0300

    Se creó un getter para la fecha de actualización y para obtener los opcionales

--- server/src/main/java/com/agro/feature/product/domain/Product.java
+++ server/src/main/java/com/agro/feature/product/domain/Product.java
@@ -14,6 +14,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -55,6 +56,7 @@ public class Product {
     @Getter
     private Double freight;
 
+    @Getter
     @CreationTimestamp
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
@@ -137,5 +139,9 @@ public class Product {
 
     void addOptional(Optional optional) {
         optionals.add(optional);
+    }
+
+    public List<Optional> getOptionals() {
+        return optionals.stream().toList();
     }
 }
